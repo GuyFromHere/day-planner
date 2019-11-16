579b8fe2593bd74d9da4c05421850a99196cd88b
Dan Dismuke <dandismuke@gmail.com>
2019-11-15 23:15:46 -0800

    Initial commit. Basic document.ready is up and running.

---
+++ javascript.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  // build day
+  // getting an error with moment so using Date for now...
+  //var currHour = moment.hour();
+  var currHour = new Date();
+  currHour = currHour.getHours();
+
+  // use static array for now. use moment methods later.
+  var hours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+
+  for (var i = 0; i < hours.length; i++) {
+    var newRow = $("<div>");
+    var newHour = $("<div>");
+    var newTimeBlock = $("<div>");
+    var newSaveBtn = $("<div>");
+    var newSaveA = $("<a>");
+    var newSaveIcon = $("<span>");
+
+    newRow.addClass("row");
+
+    newHour.addClass("hour col-md-2");
+    if (i > 2) {
+      newHour.text(hours[i] + "PM");
+    } else {
+      newHour.text(hours[i] + "AM");
+    }
+    newRow.append(newHour);
+
+    if (hours[i] < currHour) {
+      newTimeBlock.addClass("time-block past col-md-8");
+    } else if (hours[i] === currHour) {
+      newTimeBlock.addClass("time-block present col-md-8");
+    } else if (hours[i] > currHour) {
+      newTimeBlock.addClass("time-block future col-md-8");
+    }
+    newRow.append(newTimeBlock);
+
+    newSaveBtn.addClass("save-btn col-md-2");
+    newSaveA.attr("href", "#");
+    newSaveBtn.append(newSaveA);
+    newSaveIcon.addClass("glyphicon glyphicon-floppy-disk");
+    newSaveA.append(newSaveIcon);
+    newRow.append(newSaveBtn);
+
+    $(".container").append(newRow);
+  }
+});
