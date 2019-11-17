09631dd7e3aaad2fea509ca98534f6cd8047abce
Dan Dismuke <dandismuke@gmail.com>
2019-11-16 21:21:13 -0800

    Saving objects to localStorage. Rebuilt onload events with moment instead of static array. Time classes should be working now.

--- javascript.js
+++ javascript.js
@@ -1,51 +1,62 @@
 $(document).ready(function() {
-  // build day
-  var currHour = moment().hour();
-  //var currHour = new Date();
-  //currHour = currHour.getHours();
+  function buildDay() {
+    // get 8AM
+    var hour = moment()
+      .set("hour", 8)
+      .startOf("hour");
+    // get current hour
+    var currHour = moment().startOf("hour");
 
-  // use static array for now. use moment methods later.
-  var hours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+    for (var i = 0; i < 10; i++) {
+      var newRow = $("<div>");
+      var newHour = $("<div>");
+      var newTimeBlock = $("<div>");
+      var newTextArea = $("<textarea>");
+      var newSaveBtn = $("<div>");
+      newRow.addClass("row");
+      newHour.addClass("hour col-md-2");
+      newTimeBlock.addClass("time-block");
 
-  for (var i = 0; i < hours.length; i++) {
-    var newRow = $("<div>");
-    var newHour = $("<div>");
-    var newTimeBlock = $("<div>");
-    var newTextArea = $("<textarea>");
-    var newSaveBtn = $("<div>");
+      // label hour
+      newHour.text(hour.format("ha"));
 
-    newRow.addClass("row");
+      // add hour to row
+      newRow.append(newHour);
 
-    newHour.addClass("hour col-md-2");
-    if (i > 2) {
-      newHour.text(hours[i] + "PM");
-    } else {
-      newHour.text(hours[i] + "AM");
+      newTextArea.attr("id", "hour" + hour.hour());
+      // add textarea to time-block
+      newTimeBlock.append(newTextArea);
+
+      if (hour.isBefore(currHour)) {
+        //console.log(hour + " is before " + currHour);
+        newTimeBlock.addClass("time-block past col-md-8");
+      } else if (hour.isSame(currHour)) {
+        //console.log(hour + " is same " + currHour);
+        newTimeBlock.addClass("time-block present col-md-8");
+      } else if (hour.isAfter(currHour)) {
+        //console.log(hour + " is after " + currHour);
+        newTimeBlock.addClass("time-block future col-md-8");
+      }
+      newRow.append(newTimeBlock);
+
+      newSaveBtn.addClass("saveBtn col-md-2");
+      newSaveBtn.attr("id", hour.hour());
+      newSaveBtn.html('<i class="far fa-calendar-plus"></i>');
+      newRow.append(newSaveBtn);
+      // next hour
+      hour.add(1, "hour").format("ha");
+      $(".container").append(newRow);
     }
-    newRow.append(newHour);
-
-    newTextArea.attr("id", "hour" + hours[i]);
-    newTimeBlock.append(newTextArea);
-
-    if (hours[i] < currHour) {
-      newTimeBlock.addClass("time-block past col-md-8");
-    } else if (hours[i] === currHour) {
-      newTimeBlock.addClass("time-block present col-md-8");
-    } else if (hours[i] > currHour) {
-      newTimeBlock.addClass("time-block future col-md-8");
-    }
-    newRow.append(newTimeBlock);
-
-    newSaveBtn.addClass("saveBtn col-md-2");
-    newSaveBtn.attr("id", hours[i]);
-    newSaveBtn.html('<i class="far fa-calendar-plus"></i>');
-    newRow.append(newSaveBtn);
-
-    $(".container").append(newRow);
   }
 
+  // build day
+  buildDay();
+
   $(".saveBtn").on("click", function() {
-    var ta = $(this).closest(".time-block");
-    console.log(ta.attr("id"));
+    var hourId = $(this).attr("id");
+    var task = $("#hour" + hourId).val();
+    var obj = { hour: hourId, task: task };
+
+    localStorage.setItem(hourId, JSON.stringify(obj));
   });
 });
